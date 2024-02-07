6b67dff3fb781f022dbf8698be7f49926aca39a0
waltertaya <waltermittybecker5@gmail.com>
2024-02-07 10:37:28 +0300

    Updated models

--- altar/church/admin.py
+++ altar/church/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Member, Role, Consultation, SundaySchedule, Sermon, Event
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(Role)
+admin.site.register(Consultation)
+admin.site.register(SundaySchedule)
+admin.site.register(Sermon)
+admin.site.register(Event)
--- altar/church/models.py
+++ altar/church/models.py
@@ -1,5 +1,62 @@
 from django.db import models
-# from embed_video.fields import EmbedVideoField
 
-# class YourModel(models.Model):
-#     video = EmbedVideoField()
+
+class Member(models.Model):
+    member_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Role(models.Model):
+    role_id = models.AutoField(primary_key=True)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    roles = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.member.name} - {self.roles}"
+
+
+class Consultation(models.Model):
+    consult_id = models.AutoField(primary_key=True)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class SundaySchedule(models.Model):
+    schedule_id = models.AutoField(primary_key=True)
+    time = models.DateTimeField()
+    schedules = models.TextField()
+
+    def __str__(self):
+        return f"{self.time} - {self.schedules}"
+
+
+class Sermon(models.Model):
+    sermon_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    video_link = models.URLField()
+    date = models.DateField()
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    image = models.ImageField(upload_to='event_images/')
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
